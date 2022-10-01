faf2ce0bb0ce5aebead9f78ac9932782256e8f90
Ali Bahari <alibahari007@gmail.com>
2022-10-01 23:41:55 +0330

    Add code and notes for setting up a provider

--- blog-app/App.js
+++ blog-app/App.js
@@ -1,14 +1,43 @@
+import React from "react";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 
-const navigator = createStackNavigator({
+import indexScreen from "./src/screens/indexScreen";
+import { blogProvider } from "./src/context/blogContext";
 
+const navigator = createStackNavigator({
+    index: indexScreen
   },
   {
+    initialRouteName: "index",
     defaultNavigationOptions: {
-      title: "Blog"
+      title: "Blogs"
     }
   }
 );
 
-export default createAppContainer(navigator);
+const App = createAppContainer(navigator);
+
+export default () => {
+  return(
+    // Here <App /> will be passed as children prop to blogProvider!
+    // This way <App /> which is all of our program (react stack navigator) is wrapped in a provder! Genius!!!
+    <blogProvider>
+      <App />
+    </blogProvider>
+  )
+}
+
+/*
+  Provider is responsible for managing all data and necessarily won't show anything on the screen!
+  We will centralize all of our data inside the provider! This is called global state management!
+  Every component can have DIRECT access to the data!
+  Redux is a lib that does the same thing but here we are rebuilding somewhat of a Redux from scratch!
+
+  Context is somewhat like props
+  Props are used for communication between a parent and its IMMEDIATE child
+  So for communication down a number of layers we have to code a lot! Each layer we have to just pass it the next one! This sucks!!!
+  Context is here to solve this very issue!
+  Context is used for moving info from a parent to some NESTED child! Directly!
+
+*/
---
+++ blog-app/src/context/blogContext.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const blogContext = React.createContext();
+
+export const blogProvider = ({ children }) => {
+    return(
+        <blogContext.Provider>
+            {children}
+        </blogContext.Provider>
+    )
+}
+
+/*
+    Context is like a pipe! It's responsible for direct communication of provider with nested childs!
+    
+    An element when wrapped with for example <customComponent> </customComponent> tag will be passed as a prop to customComponent!!! Weird I know!!!
+    The prop is named children!
+
+    We can have many exports in one file but only one can be default! That is the one the file will be known with!
+    When just export is used when importing we have to use {}! 
+*/
