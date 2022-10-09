9b4888f04f727fe1b53dd57c9aa6f66305b94754
Ali Bahari <alibahari007@gmail.com>
2022-10-09 19:20:10 +0330

    Add code for monogoDB connection setup

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,6 +1,20 @@
+const dotenv = require("dotenv")
+dotenv.config("../.env")
 const express = require("express")
+const mongoose = require("mongoose")
 
 const app = express()
+
+mongoose.connect(process.env.MONGO_URI)
+
+mongoose.connection.on("connected", () => {
+    console.log("Connected to MongoDB instance!")
+})
+
+mongoose.connection.on("error", (err) => {
+    console.log("Error connecting to MongoDB instance: " + err)
+})
+
 
 app.get("/", (req, res) => {
     res.send("Well Hello There!")
@@ -15,4 +29,6 @@ app.listen(3000, () => {
     This is also what happens for listen!
 
     When we want to run the server we will simply run "node src/index.js" in terminal
+
+    For mongoURI we have to add our username and password to the connection string.
 */
